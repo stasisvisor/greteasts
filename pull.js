90a24130876ee9bb7d7347c6b5b2105080b81825
cabal-shred <cabal-shred.0q@icloud.com>
2024-07-14 10:10:00 +0500

    Fixed bug in role management

---
+++ pull.js
@@ -0,0 +1,10 @@
+const memoize = (fn) => {
+  const cache = new Map();
+  return (...args) => {
+    const key = JSON.stringify(args);
+    if (cache.has(key)) return cache.get(key);
+    const result = fn.apply(this, args);
+    cache.set(key, result);
+    return result;
+  };
+};
